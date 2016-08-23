d1ae2d1e16429ba63778a271e32a12de5b4dccc3
simplydesigner <simplydesigner@ya.ru>
2016-08-23 07:56:32 +0300

    Initial Commit

---
+++ hello_thread/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_thread
+//
+//  Created by simplydesigner on 8/23/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
